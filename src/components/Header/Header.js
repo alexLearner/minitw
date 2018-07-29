8ac195685f6b16018d30a3a915be20f5a41d477e
Alex Maltsev <alexq.dev@gmail.com>
2018-07-29 05:48:18 +0300

    fixed Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,42 +1,42 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+import { bindActionCreators } from "redux";
 import { ROOT } from "../../config";
+import { connect } from "react-redux";
+import { getPosts } from "../../actions/posts";
+import { open } from "../../actions/modals";
 import "./Header.css";
 
-const LINKS = [
-  {
-    to: "/",
-    text: "Users",
-    exact: true,
-  },
-  {
-    to: "/create",
-    text: "Sign In",
-  },
-];
-
-const Header = () => (
+const Header = ({ open }) => (
   <header className="header">
     <div className="container header_container">
       <Link className="header_logo" to={ROOT + "/"}>MiniTW</Link>
 
       <nav className="header_nav">
-        {
-          LINKS.map(link => (
-            <NavLink
-              className="header_nav_item"
-              activeClassName="active"
-              key={link.to}
-              to={ROOT + link.to}
-              exact={link.exact}
-            >
-              {link.text}
-            </NavLink>
-          ))
-        }
+        <NavLink
+          className="header_nav_item"
+          activeClassName="active"
+          to={ROOT + "/"}
+          exact={true}
+        >
+          Users
+        </NavLink>
+
+        <span
+          className="header_nav_item"
+          onClick={() => open("sign_in")}
+        >
+          Sign in
+        </span>
+
       </nav>
     </div>
   </header>
 );
 
-export default Header;
+export default connect(
+  null,
+  dispatch => ({
+    open: bindActionCreators(open, dispatch)
+  })
+)(Header);
